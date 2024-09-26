db30beff701eace48b9bb69ee46332eef2849620
gyuwon <hangw2130@gmail.com>
2024-09-26 13:33:02 +0900

    sg-hello first commit

---
+++ src/test/java/com/hangw/hello/SgHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangw.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
